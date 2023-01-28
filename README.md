e4b23fa077de8058af0d94b88bfcb359a1f2e7f8
Fatboy <fatboyhomenetwork@gmail.com>
2023-01-28 14:13:35 +1100

    .

--- README.md
+++ README.md
@@ -8,6 +8,6 @@ The following network services are provided:
 2. Network wide VPN proxy service [network_vpn_proxy](.\network_vpn_proxy\README.md)
 3. A xxx standard Torrent Service accessible via web interface and available to all users within the network [torrent_service_wrapper](.\torrent_service_wrapper\README.md)
 5. Network wide file share: \Public, & \Public\Temporary available to all users within the network [public_file_share_config](.\public_file_share_config\README.md)
-6. Network user home directory creating and hosting. Create  home directory. Migrating from local to network home directory. configure windows security to  control access. [network_home_dir](.\network_home_dir\README.md)
+6. Network user home directory creating and hosting. Create  home directory. Migrating from local to network home directory. configure windows security to  control access. [network_home_dir](.\network_home_folder\README.md)
 4. Network wide off site back up services. Schedule backups of folders within the network. Recovery in place. Recovery from disaster. [off_site_backup_config](.\off_site_backup_config\README.md)
 
---
+++ fatboy_sync/test__fatboy_sync.py
@@ -0,0 +1,19 @@
+import fatboy_sync
+
+# fatboy_sync.run('\\\\SERVER\\Users\\Paul\\Projects\\Fat Boy Home Network (FBHN)\\03 Network Services\\fatboy sync\\empty_source', 
+#                 '\\\\SERVER\\Users\\Paul\\Projects\\Fat Boy Home Network (FBHN)\\03 Network Services\\fatboy sync\\empty',
+#                 '\\\\SERVER\\Users\\Paul\\Projects\\Fat Boy Home Network (FBHN)\\03 Network Services\\fatboy_sync.log'
+#                )
+
+fatboy_sync.run('\\\\SERVER\\Users\\Paul\\Projects\\Fat Boy Home Network (FBHN)\\03 Network Services\\fatboy sync\\source', 
+                '\\\\SERVER\\Users\\Paul\\Projects\\Fat Boy Home Network (FBHN)\\03 Network Services\\fatboy sync\\image',
+                '\\\\SERVER\\Users\\Paul\\Projects\\Fat Boy Home Network (FBHN)\\03 Network Services\\fatboy_sync.log'
+                )
+
+# fatboy_sync.run('\\\\SERVER\\Users\\Paul', 
+#                 '\\\\SERVER\\Users\\Paul\\Projects\\Fat Boy Home Network (FBHN)\\03 Network Services\\fatboy sync\\image',
+#                 '\\\\SERVER\\Users\\Paul\\Projects\\Fat Boy Home Network (FBHN)\\03 Network Services\\fatboy_sync.log'
+#                 )
+
+print('done')
+
